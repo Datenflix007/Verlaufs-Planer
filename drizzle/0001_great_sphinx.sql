8213650d910dc5b65b62311370244833829b18a1
Felix Staacke <felix.staacke@uni-jena.de>
2026-09-12 18:50:28 +0200

    backToKonzept and removed all slob code

--- drizzle/0001_great_sphinx.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `curriculum_sources` ADD `review_note` text;--> statement-breakpoint
-ALTER TABLE `curriculum_sources` ADD `reviewed_at` text;
--- drizzle/0002_dry_vargas.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `curriculum_validity_rules` ADD `origin` text DEFAULT 'imported' NOT NULL;
